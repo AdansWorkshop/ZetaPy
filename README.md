40262f3b36ce91a10738564a3a4cf6f0ad4fbcb5
Adan's Workshop <74674130+AdansWorkshop@users.noreply.github.com>
2022-06-21 08:06:19 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -5,15 +5,46 @@ A custom language with a parser made in python
 Python >= 3.7
 
 # Methods
-in {varName} - Gets input from the terminal
-
-out {operator} {value1} {value2} - Evaluates equation and prints answer
+out {operator} {value1} {value2} values can be args variables or numbers - Evaluates equation and prints answer
 
 out {text} - Prints text
 
-set {varName} {value} - Value can be rand {start} {stop} or iter {values seperated by space} - sets the variable name to equal the value
+out {args} - Prints args from "each" for example
+
+set {varName} {value} - Value can be rand {start} {stop}, index {listName} {index}, in {prompt}, mathematics, or iter {values seperated by space} - sets the variable name to equal the value
 
 unset {varName} - Unsets variable
 
 each {listName} {codeToRun} - For loop. args equals the current value in the list
 
+each {amount of times to run} {codeToRun} - For loop. args equals the current amount of times the loop has been run
+
+incremental controls {varName} ++, {varName} --, {varName} += {increment value}, and {varName} -= {decrement value}
+
+# Example
+code:
+
+set list iter 1 2 3 4 5
+each list out ** args
+
+result:
+
+1
+4
+9
+16
+25
+-----------------------------------------------------------------------------------
+code:
+
+set n 81
+out n
+out sqrt n
+set m sqrt n
+out sqrt m
+
+result:
+
+81
+9
+2
--- main.py
+++ main.py
@@ -1,28 +1,127 @@
 import random
-from typing import List
+from os.path import exists
 
-command = input("zetapy> ")
-filename = ""
+running = False
+
 file = None
 fileContents = []
 stack = {}
-filename = command
-file = open(filename, "r")
-print("\n------------------------------------------------------------------\n")
 
 def set(lineWords, line, args, stack, j):
     if lineWords[j + 2] == "rand":
         stack[lineWords[j + 1]] = random.randint(int(lineWords[j + 3]), int(lineWords[j + 4]))
     elif lineWords[j + 2] == "iter":
-        list = []
+        iter = []
         for k in range(3, len(line.split(" "))):
             if lineWords[k].isnumeric():
-                list.append(float(lineWords[k]))
+                iter.append(float(lineWords[k]))
             elif lineWords[k] == "args":
-                list.append(args)
+                iter.append(args)
             else:
-                list.append(lineWords[k])
-        stack[lineWords[j + 1]] = list
+                iter.append(lineWords[k])
+        stack[lineWords[j + 1]] = iter
+    elif lineWords[j + 2] == "index":
+        if lineWords[j + 3] in stack.keys():
+            iter = stack.get(lineWords[j + 3])
+            if isinstance(iter, list):
+                tmp = None
+                if int(lineWords[j + 4]) < len(iter):
+                    tmp = iter[int(lineWords[j + 4])]
+                stack[lineWords[j + 1]] = tmp
+    elif lineWords[j + 2] == "in":
+        tmp = input(line[2:])
+        if tmp.isnumeric():
+            stack[lineWords[j + 1]] = float(tmp)
+        else:
+            stack[lineWords[j + 1]] = tmp
+    elif lineWords[j + 2] == "+":
+        a = None
+        b = None
+        if lineWords[j + 3] in stack.keys():
+            a = stack.get(lineWords[j + 3])
+        elif lineWords[j + 3] == "args":
+            a = args
+        elif lineWords[j + 3].isnumeric():
+            a = float(lineWords[j + 2])
+        if lineWords[j + 4] in stack.keys():
+            b = stack.get(lineWords[j + 4])
+        elif lineWords[j + 4] == "args":
+            b = args
+        elif lineWords[j + 4].isnumeric():
+            b = float(lineWords[j + 4])
+        if not a == None and not b == None:
+            stack[lineWords[j + 1]] = (a + b)
+    elif lineWords[j + 2] == "-":
+        a = None
+        b = None
+        if lineWords[j + 3] in stack.keys():
+            a = stack.get(lineWords[j + 3])
+        elif lineWords[j + 3] == "args":
+            a = args
+        elif lineWords[j + 3].isnumeric():
+            a = float(lineWords[j + 2])
+        if lineWords[j + 4] in stack.keys():
+            b = stack.get(lineWords[j + 4])
+        elif lineWords[j + 4] == "args":
+            b = args
+        elif lineWords[j + 4].isnumeric():
+            b = float(lineWords[j + 4])
+        if not a == None and not b == None:
+            stack[lineWords[j + 1]] = (a - b)
+    elif lineWords[j + 2] == "*":
+        a = None
+        b = None
+        if lineWords[j + 3] in stack.keys():
+            a = stack.get(lineWords[j + 3])
+        elif lineWords[j + 3] == "args":
+            a = args
+        elif lineWords[j + 3].isnumeric():
+            a = float(lineWords[j + 3])
+        if lineWords[j + 4] in stack.keys():
+            b = stack.get(lineWords[j + 4])
+        elif lineWords[j + 4] == "args":
+            b = args
+        elif lineWords[j + 4].isnumeric():
+            b = float(lineWords[j + 4])
+        if not a == None and not b == None:
+            stack[lineWords[j + 1]] = (float(a) * float(b))
+    elif lineWords[j + 2] == "/":
+        a = None
+        b = None
+        if lineWords[j + 3] in stack.keys():
+            a = stack.get(lineWords[j + 3])
+        elif lineWords[j + 3] == "args":
+            a = args
+        elif lineWords[j + 3].isnumeric():
+            a = float(lineWords[j + 3])
+        if lineWords[j + 4] in stack.keys():
+            b = stack.get(lineWords[j + 4])
+        elif lineWords[j + 4] == "args":
+            b = args
+        elif lineWords[j + 4].isnumeric():
+            b = float(lineWords[j + 4])
+        if not a == None and not b == None:
+            stack[lineWords[j + 1]] = (float(a) / float(b))
+    elif lineWords[j + 2] == "**":
+        if lineWords[j + 3] in stack.keys():
+            stack[lineWords[j + 1]] = (float(stack.get(lineWords[j + 3])) ** 2)
+        elif lineWords[j + 3].isnumeric():
+            stack[lineWords[j + 1]] = (float(lineWords[j + 3]) ** 2)
+    elif lineWords[j + 2] == "***":
+        if lineWords[j + 3] in stack.keys():
+            stack[lineWords[j + 1]] = (float(stack.get(lineWords[j + 3])) ** 3)
+        elif lineWords[j + 3].isnumeric():
+            stack[lineWords[j + 1]] = (float(lineWords[j + 3]) ** 3)
+    elif lineWords[j + 2] == "sqrt":
+        if lineWords[j + 3] in stack.keys():
+            stack[lineWords[j + 1]] = (float(stack.get(lineWords[j + 3])) ** .5)
+        elif lineWords[j + 3].isnumeric():
+            stack[lineWords[j + 1]] = (float(lineWords[j + 3]) ** .5)
+    elif lineWords[j + 2] == "cbrt":
+        if lineWords[j + 3] in stack.keys():
+            stack[lineWords[j + 1]] = (float(stack.get(lineWords[j + 3])) ** (1/3))
+        elif lineWords[j + 3].isnumeric():
+            stack[lineWords[j + 1]] = (float(lineWords[j + 3]) ** (1/3))
     else:
         if lineWords[j + 2].isnumeric():
             stack[lineWords[j + 1]] = float(lineWords[j + 2])
@@ -30,8 +129,6 @@ def set(lineWords, line, args, stack, j):
             stack[lineWords[j + 1]] = lineWords[j + 2]
 def unset(lineWords, stack, i, j):
     stack.pop(lineWords[j + 1])
-def setin(lineWords, stack, i, j):
-    stack[lineWords[j + 1]] = input()
 def out(lineWords, line, stack, j, args):
     if lineWords[j + 1] in stack.keys():
         print(stack.get(lineWords[j + 1]))
@@ -127,26 +224,6 @@ def out(lineWords, line, stack, j, args):
         print(args)
     elif lineWords[j + 1] != "args": 
         print(line.split("out ")[1])
-def adde(lineWords, stack, j):
-    if lineWords[j + 1] in stack.keys():
-        stack[lineWords[j - 1]] += stack.get(lineWords[j + 1])
-    else:
-        stack[lineWords[j - 1]] += float(lineWords[j + 1])
-def sube(lineWords, stack, j):
-    if lineWords[j + 1] in stack.keys():
-        stack[lineWords[j - 1]] -= stack.get(lineWords[j + 1])
-    else:
-        stack[lineWords[j - 1]] -= float(lineWords[j + 1])
-def multe(lineWords, stack, j):
-    if lineWords[j + 1] in stack.keys():
-        stack[lineWords[j - 1]] *= stack.get(lineWords[j + 1])
-    else:
-        stack[lineWords[j - 1]] *= float(lineWords[j + 1])
-def dive(lineWords, stack, j):
-    if lineWords[j + 1] in stack.keys():
-        stack[lineWords[j - 1]] /= stack.get(lineWords[j + 1])
-    else:
-        stack[lineWords[j - 1]] /= float(lineWords[j + 1])
 def each(lineWords, line, stack, j):
     if lineWords[j + 1] in stack.keys():
         iter = stack.get(lineWords[j + 1])
@@ -154,6 +231,10 @@ def each(lineWords, line, stack, j):
             for k in range(0, len(iter)):
                 rest_of_line = line[2:]
                 parse(rest_of_line, iter[k])
+    elif lineWords[j + 1].isnumeric():
+        for k in range(int(lineWords[j + 1])):
+            rest_of_line = line[2:]
+            parse(rest_of_line, k+1)
 def add(lineWords, args, stack, j):
     if lineWords[j - 1] in stack:
         iter = stack.get(lineWords[j - 1])
@@ -192,32 +273,72 @@ def rem(lineWords, args, stack, j):
             else:
                 iter.remove(lineWords[j + 1])
             stack[lineWords[j - 1]] = iter
+def inc(lineWords, i):
+    if lineWords[i - 1] in stack.keys():
+        tmp = stack.get(lineWords[i - 1])
+        if isinstance(tmp, int) or isinstance(tmp, float):
+            tmp += 1
+        stack[lineWords[i - 1]] = tmp
+def dec(lineWords, i):
+    if lineWords[i - 1] in stack.keys():
+        tmp = stack.get(lineWords[i - 1])
+        if isinstance(tmp, int) or isinstance(tmp, float):
+            tmp -= 1
+        stack[lineWords[i - 1]] = tmp
+def incV(lineWords, i):
+    if lineWords[i - 1] in stack.keys():
+        tmp = stack.get(lineWords[i - 1])
+        if isinstance(tmp, int) or isinstance(tmp, float):
+            if lineWords[i + 1].isnumeric():
+                tmp += float(lineWords[i + 1])
+        stack[lineWords[i - 1]] = tmp
+def decV(lineWords, i):
+    if lineWords[i - 1] in stack.keys():
+        tmp = stack.get(lineWords[i - 1])
+        if isinstance(tmp, int) or isinstance(tmp, float):
+            if lineWords[i + 1].isnumeric():
+                tmp -= float(lineWords[i + 1])
+        stack[lineWords[i - 1]] = tmp
+
 def parse(line, args):
     lineWords = line.split(" ")
     for j in range(0, len(lineWords)):
         if lineWords[j] == "out":
             out(lineWords, line, stack, j, args)
-        if lineWords[j] == "in":
-            setin(lineWords, stack, j)
         if lineWords[j] == "set":
             set(lineWords, line, args, stack, j)
         if lineWords[j] == "unset":
             unset(lineWords, stack, j)
-        if lineWords[j] == "+=":
-            adde(lineWords, stack, j)
-        if lineWords[j] == "-=":
-            sube(lineWords, stack, j)
-        if lineWords[j] == "*=":
-            multe(lineWords, stack, j)
-        if lineWords[j] == "/=":
-            dive(lineWords, stack, j)
         if lineWords[j] == "each":
             each(lineWords, line, stack, j)
         if lineWords[j] == "add":
             add(lineWords, args, stack, j)
         if lineWords[j] == "rem":
             rem(lineWords, args, stack, j)
-if not file == None:
-    fileContents = file.read().split("\n")
-    for i in range(0, len(fileContents)):
-        parse(fileContents[i], None)        
+        if lineWords[j] == "++":
+            inc(lineWords, j)
+        if lineWords[j] == "--":
+            dec(lineWords, j)
+        if lineWords[j] == "+=":
+            incV(lineWords, j)
+        if lineWords[j] == "-=":
+            decV(lineWords, j)
+
+def run():
+    command = input("zetapy> ").split(" ")
+    if exists(command[0]):
+        file = open(command[0], "r")
+
+    if len(command) >= 2:
+        if command[1] == "run":
+            if not file == None:
+                fileContents = file.read().split("\n")
+                for i in range(0, len(fileContents)):
+                    parse(fileContents[i], None)      
+        if command[1] == "read":
+            if not file == None:
+                print(file.read())
+    else:
+        print("command required")
+        run()
+run()
